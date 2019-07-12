022549f8133739f693962dae67e248b5552d4985
Ravindu Kandana <ravindu@Ravindus-MacBook-Pro.local>
2019-07-12 18:02:20 -0400

    fixed max_length

--- core/migrations/0002_auto_20190712_2108.py
+++ core/migrations/0002_auto_20190712_2108.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='tutorinformation',
             name='phone_number',
-            field=models.CharField(default='12049956727', max_length=10),
+            field=models.CharField(default='12049956727', max_length=11),
             preserve_default=False,
         ),
         migrations.AddField(
